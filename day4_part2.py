6170f5ab900ce6dbb48392a56db2f9a19f491f8f
Bioriginal <contact.cmeunier@gmail.com>
2022-12-04 19:55:31 +0100

    remove useless if

--- day4_part2.py
+++ day4_part2.py
@@ -10,15 +10,8 @@ for sections in camp:
     elfRange1 = [int(x) for x in elfSections[0].split('-')]
     elfRange2 = [int(x) for x in elfSections[1].split('-')]
 
-    if elfRange1[0] == elfRange1[1]:
-        elf1List = [elfRange1[0]]
-    else:
-        elf1List = [x for x in range(elfRange1[0],elfRange1[1]+1)]
-
-    if elfRange2[0] == elfRange2[1]:
-        elf2List = [elfRange2[0]]
-    else:
-        elf2List = [x for x in range(elfRange2[0],elfRange2[1]+1)]
+    elf1List = [x for x in range(elfRange1[0],elfRange1[1]+1)]
+    elf2List = [x for x in range(elfRange2[0],elfRange2[1]+1)]
 
     #https://techbeamers.com/program-python-list-contains-elements/
     check =  any(item in elf1List for item in elf2List) or any(item in elf2List for item in elf1List)
